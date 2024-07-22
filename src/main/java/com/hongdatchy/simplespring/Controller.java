7fea078a93035abb6648f2d4c9c73c91e9fdbb94
hongdatchy <hongdatchy@gmail.com>
2024-07-23 02:44:13 +0700

    s

--- src/main/java/com/hongdatchy/simplespring/Controller.java
+++ src/main/java/com/hongdatchy/simplespring/Controller.java
@@ -9,6 +9,6 @@ public class Controller {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello cc cc cc cccccccccccccccc 1234";
+        return "Hello cc cc cc cccccccccccccccc";
     }
 }
